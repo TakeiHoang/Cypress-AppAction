bc5d2c57f14b165eabf1b785c42ba4a387927d22
TakeiHoang <vhx.hoang@gmail.com>
2023-02-08 01:28:41 +0700

    Update products.cy.js

--- cypress/e2e/regression/saucedemo/products.cy.js
+++ cypress/e2e/regression/saucedemo/products.cy.js
@@ -1,19 +1,14 @@
-require("../../support/commands")
+require("../../../support/regressions")
 const account = 'standard_user'
 const password = 'secret_sauce'
 
 describe('SauceDemo Product Page Test', () => {
     beforeEach(() => {
-        cy.clearAllCookies()
-        cy.clearAllLocalStorage()
-        cy.clearAllSessionStorage()
-        cy.log('Before Each')
-        cy.visit('https://www.saucedemo.com/')
+        
     })
 
     it('Product Page', () => {
-        cy.login('standard_user', 'secret_sauce')
-        cy.get('#inventory_container').should('be.visible')
+        
     })
 
     afterEach(() => {
